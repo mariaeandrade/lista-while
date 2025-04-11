6183ca23cc5361e06020d9da7680c184b39dde8a
Andrade <dudadeandrade13@gmail.com>
2025-04-11 11:45:53 -0300

    listinha4

---
+++ 7.js
@@ -0,0 +1,6 @@
+let dinheiro = 3;
+
+while (dinheiro <= 9) {
+    dinheiro += 3;
+    console.log(`O total de moedas é ${dinheiro}`)
+}
---
+++ 8.js
@@ -0,0 +1,6 @@
+let número = 1;
+
+while (número <= 5) {
+    console.log(`O cubo de ${número} é ${número * número * número}`)
+    número++
+}
